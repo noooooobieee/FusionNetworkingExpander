411ffe0e76744dbdcfc631990e022fe1423aadf1
KitchenBoy <coleton.trenton@outlook.com>
2024-12-08 12:20:06 -0600

    Remove Dependencies

--- Mod.cs
+++ Mod.cs
@@ -4,12 +4,7 @@
     {
         public override void OnInitializeMelon()
         {
-            NetStandardLoader.Load();
-            RiptideNetworkingLoader.Load();
-
             NetworkLayer.RegisterLayersFromAssembly(Assembly.GetExecutingAssembly());
-
-            LoggerInstance.Msg("FNPlus Initialized!");
         }
 
         public override void OnLateInitializeMelon()
--- Utilites/Data/ResourcePaths.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FNPlus.Utilities
-{
-    internal static class ResourcePaths
-    {
-        // Libs
-        internal const string RiptideNetworkingPath = "FNPlus.Resources.lib.RiptideNetworking.dll";
-        internal const string netstandardPath = "FNPlus.Resources.lib.netstandard.dll";
-    }
-}
